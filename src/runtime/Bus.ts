797cd736579a8ae9a9b09d2e00b3006a25c0c08f
cruxcode <swaroopshyam0@gmail.com>
2021-11-23 17:42:29 +0530

    added Bus class

---
+++ src/runtime/Bus.ts
@@ -0,0 +1,29 @@
+import { ReplaySubjectWrapper } from "./ReplaySubjectWrapper";
+import { Observer } from "rxjs";
+
+export interface BusPostOptions {
+  onAccept?: () => void;
+  onReject?: () => {};
+}
+
+export class Bus<T, U> {
+  protected postOptions?: BusPostOptions = {};
+  private subject: ReplaySubjectWrapper<T> = new ReplaySubjectWrapper<T>();
+  constructor() {}
+  subscribe(observer: Observer<T>) {
+    return this.subject.subscribe(observer);
+  }
+  subscribeSlice(slice: (string | number)[], observer: Observer<T>) {
+    return this.subject.subscribeSlice(slice, observer.next);
+  }
+  post(type: U, data: T, options?: BusPostOptions) {
+    this.postOptions = options;
+    this.subject.next(data);
+  }
+  protected accept() {
+    if (this.postOptions?.onAccept) this.postOptions.onAccept();
+  }
+  protected reject() {
+    if (this.postOptions?.onReject) this.postOptions.onReject();
+  }
+}
