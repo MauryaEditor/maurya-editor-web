85647098f7a4d826048eb5e97eb96da28f63529c
cruxcode <swaroopshyam0@gmail.com>
2021-11-29 01:44:16 +0530

    DraggableDecorator implemented

---
+++ src/tools/design/decorators/DraggableDecorator.tsx
@@ -0,0 +1,39 @@
+/**
+    Copyright 2021 Quaffles    
+ 
+    This file is part of Maurya Editor.
+    Maurya Editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+    Maurya Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { useEffect } from "react";
+import { DesignRuntime } from "../runtime/DesignRuntime/DesignRuntime";
+import { DraggedElement } from "../runtime/interaction-states/DraggedElement";
+
+export const DraggableDecorator: React.FC<{ ID: string }> = (props) => {
+  useEffect(() => {
+    if (
+      DesignRuntime.getState()[props.ID].ref &&
+      DesignRuntime.getState()[props.ID].ref.current
+    ) {
+      const component = DesignRuntime.getState()[props.ID].ref.current!;
+      const onmousedown = (event: MouseEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+        DraggedElement.next(props.ID);
+      };
+      component.addEventListener("mousedown", onmousedown);
+      return () => {
+        component.removeEventListener("mousedown", onmousedown);
+      };
+    }
+  }, [props.ID]);
+  return <>{props.children}</>;
+};
