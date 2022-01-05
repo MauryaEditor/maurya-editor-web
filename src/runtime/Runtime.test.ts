3f1fb1d0e8927e52c75dc693e8a7ebc3de7c8c2e
cruxcode <swaroopshyam0@gmail.com>
2022-01-05 09:25:27 +0530

    onReady gets called test case added

--- src/runtime/Runtime.test.ts
+++ src/runtime/Runtime.test.ts
@@ -1,3 +1,4 @@
+import { assert } from "console";
 import { RuntimeClass } from "./Runtime";
 
 jest.mock("../lib/getAuth", () => {
@@ -18,9 +19,35 @@ jest.mock("../lib/getProjectID", () => {
   };
 });
 
+jest.mock("../api/getEvents", () => {
+  return {
+    getEvents: () => {
+      const events = require("./Runtime.events.test.json");
+      return Promise.resolve(events);
+    },
+  };
+});
+
+jest.mock("../api/getIDPool", () => {
+  return {
+    getIDPool: () => {
+      const idpool = require("./Runtime.idpool.test.json");
+      return Promise.resolve(idpool);
+    },
+  };
+});
+
+jest.mock("../api/postEvent", () => {
+  return {
+    postEvent: () => {},
+  };
+});
+
 test("onReady gets called", () => {
   const Runtime = RuntimeClass.getRuntime();
+  const isReady = jest.fn();
   Runtime.onReady(() => {
-    console.log("onReady called");
+    isReady();
   });
+  expect(isReady).toBeCalled();
 });
